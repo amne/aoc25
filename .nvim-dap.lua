28b5be50593abff9746d366835272b9de76aa53d
cornel cruceru <amne@amne.me>
2025-12-05 21:22:03 +0200

    dap config

---
+++ .nvim-dap.lua
@@ -0,0 +1,44 @@
+local dap = require("dap")
+
+
+dap.adapters.python = {
+    type = 'executable',
+    command = 'python',
+    args = { '-m', 'debugpy.adapter' }
+}
+
+dap.configurations.python = {
+    {
+        type = 'python',
+        request = 'launch',
+        name = "Launch file",
+        program = "${file}",
+        pythonPath = function()
+            return '/home/linuxbrew/.linuxbrew/bin/python3'
+        end,
+    }
+}
+
+dap.adapters.lldb = {
+    type = "executable",
+    command = "/usr/bin/lldb-vscode-14", -- adjust as needed
+    name = "lldb",
+}
+
+dap.configurations.rust = {
+   {
+      name = "wait to attach",
+      type = "lldb",
+      request = "attach",
+      program =  function()
+            -- current buffer name
+            -- return vim.fn.expand("%"):gsub("%.rs","")
+            -- only the file name
+            -- return vim.fn.expand("%:t"):gsub("%.rs","")
+            -- concat "target/debug/" with the file name
+            return "target/debug/day1"
+            -- return vim.fn.expand("%"):gsub("%.rs","")
+        end,
+      waitFor = true
+    }
+}
