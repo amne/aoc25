bee4b9bd7d004b431413aa274f7db28aa2315e89
cornel cruceru <amne@amne.me>
2025-12-13 01:17:46 +0200

    day8 p2 - easy?

--- day08/main.py
+++ day08/main.py
@@ -10,12 +10,11 @@ def compute_all_distances(junctions):
     distances.sort(key=lambda x: x[2])
     return distances
 
-def circuits_p1(junctions):
+def circuits_p1(junctions, connections = 1000):
     distances = compute_all_distances(junctions)
     print("Distances done", len(distances))
     # print(distances)
     circuits = [[c] for c in range(len(junctions))]
-    connections = 1000
     # while shortest is not None and connections > 0:
     while connections > 0:
         connections -= 1
@@ -38,12 +37,43 @@ def circuits_p1(junctions):
         # print("Connections left:", connections)
         # circuits = sorted(circuits, key=lambda x: len(x), reverse=True)
         # print("Final circuits:", circuits)
-        # shortest = shortest_distance_p1(distances, circuits)
     # print(", ".join([str(len(c)) for c in circuits]))
     circuits = sorted(circuits, key=lambda x: len(x), reverse=True)
     # print("Final circuits:", circuits)
     # print(", ".join([str(len(c)) for c in circuits]))
     print(len(circuits[0])*len(circuits[1])*len(circuits[2]))
+
+def circuits_p2(junctions):
+    distances = compute_all_distances(junctions)
+    print("Distances done", len(distances))
+    # print(distances)
+    circuits = [[c] for c in range(len(junctions))]
+    # while shortest is not None and connections > 0:
+    while len(circuits) > 1:
+        shortest = distances.pop(0)
+        extended_circuit = False
+        circuits_to_merge = []
+        for i in range(len(circuits)):
+            if shortest[0] in circuits[i]:
+                circuits_to_merge.append(i)
+            if shortest[1] in circuits[i]:
+                circuits_to_merge.append(i)
+        circuits_to_merge = list(set(circuits_to_merge))
+        # if len == 1 then both are in the same circuit
+        if len(circuits_to_merge) == 1:
+            continue
+        if len(circuits_to_merge) == 2:
+            circuits[circuits_to_merge[0]] += circuits[circuits_to_merge[1]]
+            circuits = circuits[:circuits_to_merge[1]] + circuits[circuits_to_merge[1]+1:]
+        # print("Connections left:", connections)
+        # circuits = sorted(circuits, key=lambda x: len(x), reverse=True)
+        # print("Final circuits:", circuits)
+    # print(", ".join([str(len(c)) for c in circuits]))
+    circuits = sorted(circuits, key=lambda x: len(x), reverse=True)
+    # print("Final circuits:", circuits)
+    # print(", ".join([str(len(c)) for c in circuits]))
+    # print(len(circuits[0])*len(circuits[1])*len(circuits[2]))
+    print(junctions[shortest[0]][0] * junctions[shortest[1]][0])
 
 def main():
     vec3_string = []
@@ -53,7 +83,8 @@ def main():
             vec3_string.append(line.strip().split(","))
     junctions = [(float(x), float(y), float(z)) for x, y, z in vec3_string]
     # [print(j) for j in junctions]
-    circuits_p1(junctions)
+    circuits_p1(junctions, 1000)
+    circuits_p2(junctions)
 
 
 if __name__ == "__main__":
